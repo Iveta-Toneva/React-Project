ac7e766b77311ae39c0366db3bc1225a322781c3
Iveta-Toneva <iveta.v.toneva@gmail.com>
2025-03-21 17:10:33 +0000

    Update CaricatureItem.jsx

--- client/src/components/CaricatureItem/CaricatureItem.jsx
+++ client/src/components/CaricatureItem/CaricatureItem.jsx
@@ -5,7 +5,7 @@ export function CaricatureItem(
 
     return (
         <div className="max-w-md rounded-lg overflow-hidden shadow-xl bg-white hover:shadow-2xl transform transition-all duration-300 ease-in-out hover:scale-105 cursor-pointer">
-            <img className="w-full h-72 object-cover" src={item.img} alt={item.title} />
+            <img className="w-full h-72 object-contain" src={item.img} alt={item.title} />
             <div className="p-6">
                 <div className="text-3xl font-semibold text-gray-800 truncate">{item.title}</div>
                 <div className="mt-3 text-xl text-gray-600">Likes: {item.likes}</div>
